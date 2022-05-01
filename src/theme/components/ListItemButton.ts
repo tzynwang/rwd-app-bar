0f212746a634c04696e7c0d062a7018413541afa
tzyn <tzyn.wang@gmail.com>
2022-05-01 14:56:40 +0800

    [perf] MuiListItemButton remove unused setting

--- src/theme/components/ListItemButton.ts
+++ src/theme/components/ListItemButton.ts
@@ -4,16 +4,9 @@ const l = {
       root: {
         '&.BarBodyListItemButton, &.AvatarListItemButton': {
           paddingLeft: 'calc(24px + 8px)',
-          paddingRight: 'calc(24px + 8px)'
+          paddingRight: 'calc(24px + 8px)',
         },
-        '&.ListItemCollapse': {
-          minHeight: '48px',
-          paddingLeft: 'calc((60px - 24px) / 2)'
-        },
-        // '&.BarBodyTitleRow': {
-        //   flexDirection: 'row-reverse'
-        // }
-      }
+      },
     },
   },
 };
